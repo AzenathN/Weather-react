6ce36007159c986f579f4cbde0813149eae4cfaa
AzenathN <81848056+AzenathN@users.noreply.github.com>
2022-03-01 00:43:32 -0800

    Made adjustments to api
    sometimes its the simplest answer- just hard to see.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -38,7 +38,7 @@ export default function WeatherForecast(props) {
     let apiKey = `688213a526a6897457b5169435e5ee75`;
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}$units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleForecastResponse);
 
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,24 +5,25 @@ import WeatherIcon from "./WeatherIcon";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <div className="row">
-        <div className="col-6">
+
           <h1> {props.data.city}</h1>
 
           <ul>
             <li>
-              <FormattedDate date={props.data.date} /> {props.data.description}
+              <FormattedDate date={props.data.date} />
+              </li>
+              <li className="text-capitalize">{props.data.description}
             </li>
-            <li>
-              Humidity:<strong>{props.data.humidity}%</strong>, Wind:
-              <strong>{props.data.wind}km/h</strong>
-            </li>
+
           </ul>
-        </div>
-        <div className="col-6">
-          <div className="temperature-container d-flex justify-content-end">
-            <WeatherIcon code={props.data.icon} size={52} />
-            <div>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <div className="float-left">
+                <WeatherIcon code={props.data.icon} size={52} />
+              </div>
+
+              <div className="float-left">
               <span className="temperature">
                 {Math.round(props.data.temperature)}
               </span>
@@ -30,7 +31,13 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
+        <ul>
+          <li>
+              Humidity:<strong>{props.data.humidity}%</strong>, Wind:
+              <strong>{props.data.wind}km/h</strong>
+            </li>
+        </ul>
       </div>
-    </div>
+      </div>
   );
 }
